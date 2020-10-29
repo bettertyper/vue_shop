c8b92bcf5b04f785ccf671a0f28ccbc1881c0013
bettercoder <842260931@qq.com>
2020-10-29 18:39:49 +0800

    项目搭建完成

---
+++ src/components/Login.vue
@@ -0,0 +1,32 @@
+<!-- 登录界面 -->
+<template>
+  <div class="login_container">
+    <div class="login_box">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: ''
+    
+  }
+</script>
+
+<style lang="less" scoped>
+  .login_container{
+    height: 100%;
+    background-color: #2b4b6b;
+  }
+  .login_box{
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+</style>
