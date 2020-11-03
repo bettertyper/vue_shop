ccdc54825b3d42752ce3e65a9dd4e93a31a96ebb
bettercoder <842260931@qq.com>
2020-11-03 16:08:00 +0800

    登录功能完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,15 +2,65 @@
 <template>
   <div class="login_container">
     <div class="login_box">
-
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="">
+      </div>
+      <!-- 登录表单区域 -->
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+        <el-form-item prop="username">                            <!-- 看清楚，这父传子传的是字符串username，不是变量username -->
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>        
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input type="password" v-model="loginForm.password" prefix-icon="iconfont icon-3702mima"></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>           <!-- element-ui的button组件发生点击事件时向父组件发射了名字叫"click"的自定义事件，所以这里的click不是原生事件 -->
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: ''
-    
+    name: 'Login',
+    data(){
+      return {
+        loginForm: {
+          username: 'admin',
+          password: '123456'
+        },
+        loginFormRules: {
+          username: [
+            { required: true, message: '请输入登录名称', trigger: 'blur' },
+            { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: '请输入登录密码', trigger: 'blur' },
+            { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+          ]
+        }
+      };
+    },
+    methods: {
+      //点击重置按钮，重置登录表单
+      resetLoginForm(){
+        this.$refs.loginFormRef.resetFields();
+      },
+      //点击登录按钮，进行登录
+      login(){
+        this.$refs.loginFormRef.validate(async valid => {                        //对表单进行预校验(如果所有输入框的内容符合各自的验证规则，则向callback中传入true；否则传入false)
+          if(!valid) return;
+          const {data: res} = await this.$http.post('login', this.loginForm);    //baseURL里最后有个/，所以这里可以不加/。使用async + await对promise处理，代替then。解构时可以重命名：data: res
+          if(res.meta.status !== 200) return this.$message.error('登录失败');    //使用return可以省略else
+          this.$message.success('登录成功');
+          window.sessionStorage.setItem('token', res.data.token);                 //将token保存到sessionStorage中
+          this.$router.push('/home');
+        });
+      }
+    }
   }
 </script>
 
@@ -28,5 +78,34 @@
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
+    .avatar_box{
+      height: 130px;
+      width: 130px;
+      border: 1px solid #eee;
+      border-radius: 50%;
+      padding: 10px;
+      box-shadow: 0 0 10px #ddd;
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: #fff;
+      img{
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+      }
+    }
+  }
+  .login_form{
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+  }
+  .btns{
+    display: flex;
+    justify-content: flex-end;
   }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login'
+import Home from '../components/Home'
 
 
 Vue.use(VueRouter)
@@ -13,11 +14,29 @@ const routes = [
   {
     path: '/login',
     component: Login
+  },
+  {
+    path: '/home',
+    component: Home
   }
 ]
 
 const router = new VueRouter({
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  /* if(to.path === '/login')
+    next();
+  else if(!window.sessionStorage.getItem('token')){
+    next('/login');
+  }else{
+    next();
+  } */
+  if(to.path === '/login') return next();
+  const tokenStr = window.sessionStorage.getItem('token');      //根据键名获取sessionStorage中对应的值
+  if(!tokenStr) return next('/login');
+  next();
+});
 
 export default router
