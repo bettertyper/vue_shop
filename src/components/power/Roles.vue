6c92e8bf1bbf1b668e857db17d32a6751f4a5de8
bettercoder <842260931@qq.com>
2020-11-15 16:09:01 +0800

    权限功能完成

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -35,7 +35,7 @@
                   </el-col>
                   <el-col :span="18">
                     <!-- 渲染并展示三级权限 -->
-                    <el-tag type="warning" v-for="(item3, i3) in item2.children" :key="item3.id" closable @close="removeRightById(scope.row, item3.id)">{{item3.authName}}</el-tag>
+                    <el-tag type="warning" v-for="item3 in item2.children" :key="item3.id" closable @close="removeRightById(scope.row, item3.id)">{{item3.authName}}</el-tag>
                   </el-col>
                 </el-row>
               </el-col>
